d58736c38a32268b3704eb87c7160d967d8e44b7
sengaranurag <anuragsingh450a@gmail.com>
2024-12-24 12:54:45 +0530

    creating homepage and createpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { Box , Button } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 
+import CreatePage from "./Pages/CreatePage";
+import HomePage from "./Pages/HomePage";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Box minHeight={"100vh"}>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="/create" element={<Createpage/>}/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/create" element={<CreatePage/>}/>
       </Routes>
     </Box>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { Provider } from "@/components/ui/provider";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-     <Provider>
+     <ChakraProvider>
       <App />
-     </Provider>
+     </ChakraProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
